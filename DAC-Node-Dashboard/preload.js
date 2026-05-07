0f266b584a016f19972d3e5a24940f8da9c77e78
JERUZZALEM <138619868+EdLWEISS186@users.noreply.github.com>
2026-05-07 12:43:46 +0700

    Delete DAC-Node-Dashboard/preload.js

--- DAC-Node-Dashboard/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('api', {
-  run: (scriptPath, type) => ipcRenderer.invoke('run-script', scriptPath, type),
-  stop: (type) => ipcRenderer.invoke('stop-script', type),
-  onLog: (callback) => ipcRenderer.on('log', (_, payload) => callback(payload))
-})
